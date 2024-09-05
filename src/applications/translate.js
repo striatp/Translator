de10b8afb82d5c5aa54c45cd96155ca8631196dd
Sebastien <sebastien.works@outlook.com>
2024-09-05 21:57:43 +0200

    Update translate.js

--- src/applications/translate.js
+++ src/applications/translate.js
@@ -1,1 +1,13 @@
-
+module.exports = {
+  data: {
+    name: "Translate",
+    description: "Translates the selected message into the preferred language in your settings.",
+    type: 3,
+    integration_types: [0],
+    contexts: [0, 2]
+  },
+  code: `$interactionReply[
+    $ephemeral
+    hello
+  ]`
+}
