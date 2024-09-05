f5d64eb23f0007e3c9651df610f5a9254566c778
Sebastien <sebastien.works@outlook.com>
2024-09-05 21:58:43 +0200

    Update translate.js

--- src/applications/translate.js
+++ src/applications/translate.js
@@ -1,7 +1,6 @@
 module.exports = {
   data: {
     name: "Translate",
-    description: "Translates the selected message into the preferred language in your settings.",
     type: 3,
     integration_types: [0],
     contexts: [0, 2]
