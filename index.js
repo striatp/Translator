2dfe08f64f42d5b9a1bdb455aeb8cb50bf15ff9a
Sebastien <sebastien.works@outlook.com>
2024-09-05 21:34:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-
+const { ForgeClient } = require("@tryforge/forgescript");
+const { ForgeDB } = require("@tryforge/forge.db");
+const { Token } = require("./config.local.js");
+// Client initialization
+const client = new ForgeClient({
+    "intents": [
+        "Guilds",
+        "MessageContent"
+    ],
+    "events": [
+        "debug",
+        "error",
+        "ready",
+        "shardError",
+        "shardDisconnect",
+        "shardReady",
+        "shardReconnecting",
+        "shardResume"
+    ],
+    "extensions": [
+        new ForgeDB()
+    ]
+})
+   
+// Load the commands
+client.commands.load("commands")
+client.applicationCommands.load("applications")
+   
+// Your bot token
+client.login(Token);
