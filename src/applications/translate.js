f7bd2cce2cc471b53e1f324254e8c2276a432ce3
Sebastien <sebastien.works@outlook.com>
2024-09-07 13:40:06 +0200

    Update translate.js

--- src/applications/translate.js
+++ src/applications/translate.js
@@ -8,7 +8,6 @@ module.exports = {
   code: `
   $interactionReply[
     $ephemeral
-    $!httpRequest[https://api.kastg.xyz/api/tool/translate?input=$getMessage[$djsEval[ctx.interaction.channelId];$option[message];content]&to=$locale&from=auto;GET]
-    $httpResult[result;0;output]
+    $djsEval[ctx.interaction.channelId]
   ]`
 }
