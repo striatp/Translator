f1429ceb1ba7a2fcb6342876b1dcfb3c91cb6e41
Sebastien <sebastien.works@outlook.com>
2024-09-07 21:49:09 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
-const { Token } = require("./config.local.js");
+const config = require("./config.local.js");
+const bot_token = `${config.token}`
 
 module.exports = {
-  botToken: Token,
+  botToken: bot_token,
   theme: {
     embed_colors: {
       primaryColor: "#FF3C65"
