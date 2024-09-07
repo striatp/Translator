5221172881fd90d28ad0aa9427b570665bf26664
Sebastien <sebastien.works@outlook.com>
2024-09-07 11:55:33 +0200

    Update translate.js

--- src/applications/translate.js
+++ src/applications/translate.js
@@ -8,7 +8,7 @@ module.exports = {
   code: `
   $interactionReply[
     $ephemeral
-    $!httpRequest[https://api.kastg.xyz/api/tool/translate?input=$getMessage[1270434516984402041;1281914775353294871;content]&to=$locale&from=auto;GET]
+    $!httpRequest[https://api.kastg.xyz/api/tool/translate?input=$getMessage[$channelID;$option[message];content]&to=$locale&from=auto;GET]
     $httpResult[result;0;output]
   ]`
 }
