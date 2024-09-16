2f759e7054c1acfe80318124156a147f18c2a5ea
Sebastien <sebastien.works@outlook.com>
2024-09-16 20:10:36 +0200

    Create suggestion_create.js

---
+++ src/applications/suggestion/suggestion_create.js
@@ -0,0 +1,16 @@
+module.exports = {
+  data: {
+    name: "create",
+    description: "Create a suggestion and send it.",
+    type: 1,
+    options: [
+      {
+        type: 3,
+        name: "message",
+        description: "You can write your suggestion here.",
+        required: false
+      }
+    ]
+  },
+  code: ``
+}
