04858f3ea519786666994ba4ce793afc3575246a
Sebastien <sebastien.works@outlook.com>
2024-09-28 21:11:35 +0200

    Delete src/commands directory

--- src/commands/shadowban.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  type: "messageCreate",
-  code: `
-  $let[ShadowBannedUsers;$getServerVar[ShadowBannedUsers;$guildID]]
-  $onlyIf[$includes[$get[ShadowBannedUsers];$authorID]==false;]
-
-  $deleteMessages[$channelID;$messageID]
-  `
-}
