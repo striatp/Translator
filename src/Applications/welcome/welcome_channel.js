d084a8a08e573603c5a5befa807dd14ff7ffebf2
Sebastien <sebastien.works@outlook.com>
2024-09-29 14:58:57 +0200

    Create welcome_channel.js

---
+++ src/Applications/welcome/welcome_channel.js
@@ -0,0 +1,18 @@
+
+
+module.exports = {
+  data: {
+    name: "channel",
+    description: "Set the channel where welcome messages will be sent.",
+    type: 1,
+    options: [
+      {
+        type: 7,
+        name: "channel",
+        description: "",
+        required: true
+      }
+    ]
+  },
+  code: ``
+}
