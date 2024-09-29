0362a7e95bb7ff9b1a1bcecd4b1c995d3edb2dd0
Sebastien <sebastien.works@outlook.com>
2024-09-29 15:17:27 +0200

    Update welcome_channel.js

--- src/Applications/welcome/welcome_channel.js
+++ src/Applications/welcome/welcome_channel.js
@@ -1,5 +1,6 @@
+const permission = require("../../../Ressources/discord_permissions.json");
 const configs = require("../../../config.js")
-const english = require("../../../translations/en.json");
+const english = require("../../../Ressources/Translations/en.json");
 
 module.exports = {
   data: {
@@ -16,6 +17,10 @@ module.exports = {
     ]
   },
   code: `
-$onlyIf[$hasP]
+$onlyIf[$hasPerms[$guildID;$authorID;ManageGuild]==true;
+  $ephemeral
+  $description[You need the \`ManageGuild\` permission to run this command.]
+  $color[${config.embed_colors.primary}]
+]
 `
 }
